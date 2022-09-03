cd80a5de258ad1fce7bd80a62820f544f4342cc9
Lacey Umamoto <lcumamoto@gmail.com>
2022-09-03 13:51:07 -0700

    Renamed Modals to Models, added Chat and Message models

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      require: true,
+      required: true,
       min: 3,
       max: 20,
       unique: true,
@@ -22,10 +22,15 @@ const UserSchema = new mongoose.Schema(
     },
     profilePicture: {
       type: String,
-      default: "",
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
