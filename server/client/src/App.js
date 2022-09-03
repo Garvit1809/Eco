d9a2f09695e65e4f6e145c5e31ace369f583a0d2
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-09-03 20:54:22 +0530

    Event Page

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -5,12 +5,16 @@ import Feed from './Pages/Feed';
 import Campaign from  './Components/Campaign';
 import Campaigns from './Pages/Campaigns';
 import Event from './Components/Event';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
     <Route path='/' element={<Home/>} />
+    <Route path='/register' element={<Register/>} />
+    <Route path='/login' element={<Login/>} />
     <Route path='/feed' element={<Feed/>} />
     <Route path='/campaigns' element={<Campaigns/>} />
     <Route path='/campaigns/id' element={<Campaign/>} />
