cb5b77c0cfd647b30d06c6c2b30d8a9bc6bc3242
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-09-05 05:14:13 +0530

    Final Changes

--- server/client/src/Pages/Home.js
+++ server/client/src/Pages/Home.js
@@ -36,6 +36,10 @@ div.image{
   z-index: 1;
   }
 
+  p{
+    font-size: 1.5rem;
+    font-weight: 400;
+  }
 div.text{
   position: absolute;
   background-color: rgb(0,0,0); /* Fallback color */
@@ -149,19 +153,20 @@ const Home = () => {
     <First>
     <div className='image' ></div>
     <div className='text' >
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti excepturi doloribus in maxime atque quae exercitationem nesciunt aspernatur debitis dicta.</p>
+    <p>EcoTogether attempts to address the issue of environmental sustainability through human responsibility. It provides users a social platform where they are able to share resources in order to enact change in their community, and features a tool that helps users find eco-friendly alternatives to commercial products. Through EcoTogether, we aim to bring people together in the environmental movement, and instill hope that it is never too late to save our planet.</p>
     </div>
     </First>
     <Eventss>
      <img src={socialMediaImg} alt="" />
      <div>
-     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem at ab error corrupti qui, reiciendis recusandae nobis. Laborum ad velit maxime pariatur voluptatem, autem quibusdam eveniet corrupti similique placeat exercitationem?</p>
+     <p>Users can share their contribution to environmental preservation with others. They can post upcoming campaigns and events and can collaborate with each other. The event may be a hackathon, seminar, webinar or a quiz. We imply to aware people through these events so more folks can join in to contribute to environment preservation. 
+     </p>
      <Button text="Social"   />
      </div>
      </Eventss>
      <Campaigns>
      <div>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, expedita ipsum eos fugit perspiciatis deleniti debitis vero accusantium eveniet praesentium consequatur magni temporibus libero delectus amet earum? Illum, quam labore?</p>
+     <p>They also have the ability to start campaigns and events. Other users can join these campaigns by donating in order to support the specified cause or meeting up at scheduled clean up sites. As for events, users are able to organize hackathons, seminars, and contests to spread environmental awareness.</p>
      <Button text="Events" color="#ffffff" backgroundColor="rgba(0,0,0,0.8)"/>
      </div>
      <img src={event} alt="" />
@@ -169,7 +174,7 @@ const Home = () => {
      <Ecofinder>
      <img src={subs} alt="" />
      <div>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo facere architecto, animi rerum labore corporis explicabo consequatur non, cum aliquam sit natus consectetur esse nostrum libero, culpa fugit eius quidem?</p>
+     <p>This page helps users to find eco-friendly products instead of commercial ones. For example, if the user wants to find a toothbrush, then it will give search results of environment friendly toothbrush substitutes.</p>
      <Button text="EcoFinder" />
      </div>
      </Ecofinder>
