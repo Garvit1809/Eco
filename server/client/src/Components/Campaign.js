0c3c6d1334ff174e7af1b0cc01454fa72f0149e7
Lacey Umamoto <lcumamoto@gmail.com>
2022-09-04 13:16:19 -0700

    Turn Campaign into a class

--- server/client/src/Components/Campaign.js
+++ server/client/src/Components/Campaign.js
@@ -1,32 +1,62 @@
-import React from 'react'
-import styled from 'styled-components'
-import Navbar from './Navbar'
+import React, { Component } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import Navbar from "./Navbar";
+import ChatRoom from "../Components/ChatComp/ChatRoom";
 
 const Section = styled.div`
-width: 100%;
-height: calc(100vh - 4rem);
-border: 1px solid red;
-`
+  width: 100%;
+  height: calc(100vh - 4rem);
+  border: 1px solid red;
+`;
 
 const LeftSection = styled.div`
-width: 20%;
-height: 100%;
-border: 1px solid black;
-`
+  width: 20%;
+  height: 100%;
+  border: 1px solid black;
+`;
 
-const Campaign = () => {
-  return (
-    <>
-    <Navbar/>
-    <Section>
-    <LeftSection>
-    <h2>Upcoming Events</h2>
-    <h2>Campaign Details</h2>
-    <h2>Members</h2>
-    </LeftSection>
-    </Section>
-    </>
-  )
+class Campaign extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campaign: {},
+    };
+  }
+
+  componentDidMount() {
+    console.log("Print id: " + this.props.match.params.id);
+    axios
+      .get(`/${this.props.match.params.id}`)
+      .then((res) => {
+        console.log("API-response: " + res.data);
+        this.setState({
+          campaign: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(`Campaign Error: ${err}`);
+      });
+  }
+
+  render() {
+    const campaign = this.state.campaign;
+    const chat = campaign.chat;
+
+    return (
+      <>
+        <Navbar />
+        <Section>
+          <LeftSection>
+            <h2>Upcoming Events</h2>
+            <h2>Campaign Details</h2>
+            <h2>Members</h2>
+          </LeftSection>
+          <ChatRoom chat={chat} />
+        </Section>
+      </>
+    );
+  }
 }
 
-export default Campaign
+export default Campaign;
