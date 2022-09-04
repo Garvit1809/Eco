8495e923bc73b78fdf165c84744cf711fe99fba6
Lacey Umamoto <lcumamoto@gmail.com>
2022-09-04 13:12:21 -0700

    Update server's app.js for Socket.io logic

--- server/app.js
+++ server/app.js
@@ -3,10 +3,13 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const socketIo = require("socket.io");
 
 const authRoutes = require("./Routes/authRoutes");
 const postsRoutes = require("./Routes/postsRoutes");
 const chatRoutes = require("./Routes/chatRoutes");
+const messageRoutes = require("./Routes/messageRoutes");
+const campaignRoutes = require("./Routes/campaignRoutes");
 
 const app = express();
 
@@ -19,18 +22,57 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postsRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/campaign", campaignRoutes);
 
 const PORT = process.env.PORT;
 const CONNECTION_URL = process.env.MONGO_URL;
 
+let server;
 mongoose
   .connect(CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() =>
-    app.listen(PORT, () => {
-      console.log(`Server running to port ${PORT}`);
-    })
+  .then(
+    () =>
+      (server = app.listen(PORT, () => {
+        console.log(`Server running to port ${PORT}`);
+      }))
   )
   .catch((error) => console.log(error.message));
+
+//  Socket.io
+const NEW_CHAT_MESSAGE_EVENT = "NEW_CHAT_MESSAGE_EVENT";
+const START_TYPING_MESSAGE_EVENT = "START_TYPING_MESSAGE_EVENT";
+const STOP_TYPING_MESSAGE_EVENT = "STOP_TYPING_MESSAGE_EVENT";
+
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} connected`);
+  // Join a conversation
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+  // Listen typing events
+  socket.on(START_TYPING_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(START_TYPING_MESSAGE_EVENT, data);
+  });
+  socket.on(STOP_TYPING_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(STOP_TYPING_MESSAGE_EVENT, data);
+  });
+  // Leave the room if the user closes the socket
+  socket.on("disconnect", () => {
+    socket.leave(roomId);
+  });
+});
